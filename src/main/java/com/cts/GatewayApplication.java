09663026be7a960b32f824ca7500673e98d6d666
navkk <navkk@host.docker.internal>
2021-12-02 17:59:05 +0530

    First Commit

---
+++ src/main/java/com/cts/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@RefreshScope
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
